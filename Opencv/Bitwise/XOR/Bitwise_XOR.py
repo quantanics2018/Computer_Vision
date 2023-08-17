6025c58a5612a053ea79cc38f8c6900af1b54551
Guhanapriya <104905572+Guhanapriya@users.noreply.github.com>
2023-08-17 11:21:25 +0530

    Bitwise_XOR_Operation in OpenCV

---
+++ Opencv/Bitwise/XOR/Bitwise_XOR.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+# Load two images
+image1 = cv2.imread(r"D:\OpenCV\Bitwise\XOR\Image1.jpg")
+image2 = cv2.imread(r"D:\OpenCV\Bitwise\XOR\Image2.jpg")
+
+# Check if images were loaded successfully
+if image1 is None or image2 is None:
+    print("Error loading images.")
+    exit()
+
+# Resize the images to the same dimensions (optional)
+image1 = cv2.resize(image1, (image2.shape[1], image2.shape[0]))
+
+# Perform bitwise XOR operation
+result = cv2.bitwise_xor(image1, image2)
+
+# Display the original images and the result
+cv2.imshow('Image 1', image1)
+cv2.imshow('Image 2', image2)
+cv2.imshow('Bitwise XOR Result', result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
